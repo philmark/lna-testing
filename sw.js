47c2dd58ea89a8c61f44e5449767c034191b8007
Philip Mark <phil.mark@gmail.com>
2025-12-12 13:54:10 -0800

    Contd

--- sw.js
+++ sw.js
@@ -11,6 +11,11 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
     event.waitUntil(getLNAState());
 
+    if (event.request.headers.get('X-Use-SW') !== 'true') {
+        console.log('[sw] not handling fetch, X-Use-SW header not set');
+        return;
+    }
+
     if (self.lnaPermission === 'granted') {
         console.log('[sw] handling fetch with LNA granted');
         event.respondWith(fetch(event.request));
